9647f1ffe394a30e2d6025ec9ab63fdf8fa2ac8f
svc-d-ixg-build <jan.rockemann@gmail.com>
2017-12-04 13:48:37 -0500

    Add project frame

---
+++ src/main/java/com/devopsjan/nichts/NichtsApplication.java
@@ -0,0 +1,12 @@
+package com.devopsjan.nichts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NichtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NichtsApplication.class, args);
+	}
+}
